5ca2354aece431a146aa92ab71dc527c68275560
Jaroslav Švarc <jara.svarc@email.cz>
2022-06-29 22:12:18 +0200

    Přidejte soubory projektu.

---
+++ StaticSiteGenerator/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace StaticSiteGenerator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
