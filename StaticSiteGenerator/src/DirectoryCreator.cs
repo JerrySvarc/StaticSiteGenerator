e5c813eb417b11eabcab183f6179dca4c4a063e4
Jaroslav Švarc <jara.svarc@email.cz>
2022-07-05 21:44:58 +0200

    Added comments.

--- StaticSiteGenerator/src/DirectoryCreator.cs
+++ StaticSiteGenerator/src/DirectoryCreator.cs
@@ -2,6 +2,9 @@
 {
     static class DirectoryCreator
     {
+        /// <summary>
+        /// Creates the template website directory structure.
+        /// </summary>
         public static void CreateTemplateDirectories()
         {
             string currentDirectory = Directory.GetCurrentDirectory();
@@ -18,22 +21,9 @@
             }
         }
 
-        public static void CreateNamedDirectories(string name)
-        {
-            string currentDirectory = Directory.GetCurrentDirectory();
-            string websiteDirectory = currentDirectory + "/" + name;
-            try
-            {
-                Directory.CreateDirectory(websiteDirectory);
-                Directory.CreateDirectory(websiteDirectory + "/posts");
-                Directory.CreateDirectory(websiteDirectory + "/pictures");
-            }
-            catch (Exception)
-            {
-                Console.WriteLine("An error has occured. Cannot create website directories.");
-            }
-        }
-
+        /// <summary>
+        /// Creates the output directory.
+        /// </summary>
         public static void CreateOutputDirectory()
         {
             string dirName = new DirectoryInfo(Directory.GetCurrentDirectory()).Name;
