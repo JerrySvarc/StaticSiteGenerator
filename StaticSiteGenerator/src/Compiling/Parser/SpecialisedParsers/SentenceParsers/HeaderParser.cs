cfc22200788094c7e3e1a120bf427590e80efa72
Jaroslav Švarc <jara.svarc@email.cz>
2022-07-12 21:48:10 +0200

    Created the HeaderParser.

--- StaticSiteGenerator/src/Compiling/Parser/SpecialisedParsers/SentenceParsers/HeaderParser.cs
+++ StaticSiteGenerator/src/Compiling/Parser/SpecialisedParsers/SentenceParsers/HeaderParser.cs
@@ -10,7 +10,40 @@ namespace StaticSiteGenerator
     {
         public Node Parse(List<IToken> tokens)
         {
-            
+            int hashtagCount = 0;
+            int index = 0;
+            while (tokens[index].Type == TokenType.HASHTAG && index <= 6)
+            {
+                hashtagCount++;
+            }
+            if (tokens[hashtagCount].Type == TokenType.TEXT)
+            {
+                switch (hashtagCount)
+                {
+                    case 1:
+                        return Node.NodeFactory(NodeType.H1, tokens[hashtagCount].Value, hashtagCount + 1);
+                        break;
+                    case 2:
+                        return Node.NodeFactory(NodeType.H1, tokens[hashtagCount].Value, hashtagCount + 1);
+                        break;
+                    case 3:
+                        return Node.NodeFactory(NodeType.H1, tokens[hashtagCount].Value, hashtagCount + 1);
+                        break;
+                    case 4:
+                        return Node.NodeFactory(NodeType.H1, tokens[hashtagCount].Value, hashtagCount + 1);
+                        break;
+                    case 5:
+                        return Node.NodeFactory(NodeType.H1, tokens[hashtagCount].Value, hashtagCount + 1);
+                        break;
+                    case 6:
+                        return Node.NodeFactory(NodeType.H1, tokens[hashtagCount].Value, hashtagCount + 1);
+                        break;
+                    default:
+                        return null;
+                        break;
+                }
+            }
+            return null;
         }
     }
 }
